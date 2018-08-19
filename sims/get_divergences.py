8777beb6c4436724c2713644ee0c1f3a9bc4c3ec
Peter Ralph <petrel.harp@gmail.com>
2018-08-19 08:59:21 -0700

    more lit notes

--- sims/get_divergences.py
+++ sims/get_divergences.py
@@ -41,16 +41,17 @@ outdir = args.basedir
 samples_per_cell = args.samples_per_cell
 num_subsamples = args.num_subsamples
 mutation_rate = args.mutation_rate
-num_chroms = 1
 grid_width = args.grid_width
 grid_height = args.grid_height
+num_chroms = 1
 
 def grid_samples(ts, n, m, prob=1.0, shrink=0.75):
     '''
     Returns a list of lists of node IDs, the ones that fall in the middle
     shrink of each rectangle given by discretizing into an (nxm) grid.
     '''
-    samples = [[[] for _ in range(n)] for _ in range(m)]
+    samples = [[[] for _ in range(m)] for _ in range(n)]
+    sample_inds = [[[] for _ in range(m)] for _ in range(n)]
     locs = np.array(ts.tables.individuals.location)
     locs.resize((int(len(ts.tables.individuals.location,)/3), 3))
     max_x = np.ceil(100*max(locs[:,0]))/100
@@ -65,11 +66,13 @@ def grid_samples(ts, n, m, prob=1.0, shrink=0.75):
             x_diff = ind.location[0]/dx - (i + 0.5)
             y_diff = ind.location[1]/dy - (j + 0.5)
             if (abs(x_diff) < shrink / 2) and (abs(y_diff) < shrink / 2):
-                samples[i][j].extend(ind.nodes)
+                sample_inds[i][j].append(ind)
     # put these in random order
-    for a in samples:
-        for b in a:
-            np.random.shuffle(b)
+    for i in range(n):
+        for j in range(m):
+            np.random.shuffle(sample_inds[i][j])
+            for ind in sample_inds[i][j]:
+                samples[i][j].extend(ind.nodes)
     return samples
 
 
@@ -78,21 +81,7 @@ for treefile in glob.glob(os.path.join(outdir, "*.trees")):
     logfile.flush()
     decap = pyslim.load(treefile, slim_format=True)
 
-    # recapitate
-    recomb = msprime.RecombinationMap(positions = [0.0, decap.sequence_length], 
-                                      rates = [1e-8, 0.0],
-                                      num_loci = int(decap.sequence_length))
-
-    pop_configs = [msprime.PopulationConfiguration() for _ in range(decap.num_populations)]
-
-    recap = msprime.simulate(
-                from_ts = decap, 
-                population_configurations = pop_configs,
-                recombination_map = recomb,
-                start_time = decap.slim_generation,
-                Ne=2e4)
-
-    sample_grid = grid_samples(recap, n=grid_width, m=grid_height, prob=1.0, shrink=0.75)
+    sample_grid = grid_samples(decap, n=grid_width, m=grid_height, prob=1.0, shrink=0.75)
     samples = [a for b in sample_grid for a in b]
     # extract nonoverlapping subsamples
     num_subs_per_cell = [min(samples_per_cell, int(len(a)/num_subsamples)) for a in samples]
@@ -106,13 +95,16 @@ for treefile in glob.glob(os.path.join(outdir, "*.trees")):
         samplefile.write("\t".join(['id', 'population', 'x', 'y', 'individual']) + "\n")
         for k in range(len(subsamples)):
             for a in subsamples[k]:
-                ind = recap.individual(recap.node(a).individual)
-                samplefile.write("{}\t{}\t{}\t{}\t{}\n".format(a, k, ind.location[0], ind.location[1], recap.node(a).individual))
+                ind = decap.individual(decap.node(a).individual)
+                samplefile.write("{}\t{}\t{}\t{}\t{}\n".format(a, k, ind.location[0],
+                                 ind.location[1], decap.node(a).individual))
         samplefile.close()
         # we want individual divergences
         the_subsamples = [x for y in subsamples for x in y]
         # simplify first for speed
-        sub_ts = recap.simplify(the_subsamples)
+        sub_ts = decap.simplify(the_subsamples)
+        sub_ts = pyslim.SlimTreeSequence.load_tables(sub_ts.tables)
+        recap = sub_ts.recapitate(recombination_rate=1e-9, Ne=1e3)
         mut_ts = msprime.mutate(sub_ts, rate=mutation_rate)
         # breaks at the chromosomes
         windows = np.linspace(0.0, mut_ts.sequence_length, num_chroms + 1)
