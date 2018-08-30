78d864a8a55f4fa7effc8e9e638f24c0499d41b8
Peter Ralph <petrel.harp@gmail.com>
2018-08-30 16:51:25 -0700

    plotting

--- sims/plot_locations.py
+++ sims/plot_locations.py
@@ -6,16 +6,17 @@ import glob
 matplotlib.use('Agg')
 from matplotlib import pyplot as plt
 
-outdir = "run_031087/"
-# treefiles = [outdir + "pop_{:03d}.trees".format(x) for x in range(1, 21)]
-treefiles = glob.glob(outdir + "pop_*.trees")
+treefiles = glob.glob("run_0*/*.trees")
 
 for treefile in treefiles:
-    # treefile = outdir + "pop_100.trees"
+    print(treefile)
     outfile = treefile + ".locs.png"
-    ts = pyslim.load(treefile, slim_format=True)
+    locfile = treefile + ".locs.tsv"
+    ts = msprime.load(treefile)
     locations = np.array([i.location for i in ts.individuals()])
+    np.savetxt(locfile, locations, header="x\ty\tz")
     fig = plt.figure(figsize=(max(locations[:,0]),max(locations[:,1])))
     plt.scatter(locations[:,0], locations[:,1], marker='.')
     plt.savefig(outfile, dpi=288)
+    print("... done!")
 
